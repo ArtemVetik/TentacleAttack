b3285fed116d3ca761e2ba1bd6dd95d6fc5d37d7
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-06-28 11:59:23 +0300

    Add eating area

---
+++ Assets/Scripts/Tentacles/EatingArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EatingArea : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TriggerEnter");
+
+        if(other.TryGetComponent(out Enemy enemy))
+        {
+            Destroy(enemy.gameObject);
+        }
+    }
+
+}
