5ff8727e263bb4650f473c5557da38a7eac67e89
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-06-30 15:46:55 +0300

    Build new level

---
+++ Assets/Scripts/Map/TriggerSpeaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class TriggerSpeaker : MonoBehaviour
+{
+    public event Action TriggerEnter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out TargetMovement target))
+        {
+            TriggerEnter?.Invoke();
+        }
+    }
+
+}
