a2f80e10417094349010f0a08c91e50bfb737abc
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-07-12 16:49:48 +0300

    Add MovingSawtrap

--- Assets/Scripts/Map/HidingSawTrap.cs
+++ Assets/Scripts/Map/HidingSawTrap.cs
@@ -12,10 +12,13 @@ public class HidingSawTrap : ActiveObject
 
     [SerializeField] private HideSide _hideSide;
     [SerializeField] private bool _isMoving;
+    [SerializeField] private float _speed;
     
     private Animator _selfAnimator;
     private Spline _spline;
     private Coroutine _moving;
+    private float _distanceCovered;
+    private Direction _direction;
 
     private void Awake()
     {
@@ -24,6 +27,10 @@ public class HidingSawTrap : ActiveObject
 
         if(_isMoving && _spline != null)
         {
+            _distanceCovered = 0;
+            _direction = Direction.Right;
+            transform.position = _spline.GetSampleAtDistance(_distanceCovered).location;
+
             _moving = StartCoroutine(Moving());
         }
     }
@@ -31,13 +38,23 @@ public class HidingSawTrap : ActiveObject
     public override void Action()
     {
         string stringValue = Enum.GetName(typeof(HideSide), _hideSide);
+        StopCoroutine(_moving);
         _selfAnimator.SetTrigger(stringValue);
     }
 
     private IEnumerator Moving()
-    {
+    {     
         while(true)
         {
+            _distanceCovered += _speed * Time.deltaTime * (int)_direction;
+
+            if(_distanceCovered >= _spline.Length || _distanceCovered < 0)
+            {
+                _direction = _direction == Direction.Left ? Direction.Right : Direction.Left;
+                _distanceCovered = _distanceCovered < 0 ? 0 : _spline.Length - 0.01f;
+            }
+
+            transform.position = _spline.GetSampleAtDistance(_distanceCovered).location;
 
             yield return null;
         }
