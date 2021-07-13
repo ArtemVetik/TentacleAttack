ce3c2fa97ebb042d57faa274c2cbfabe40901a3f
Artem <av.unitygames@gmail.com>
2021-07-13 20:31:01 +0300

    Fix build

--- Assets/Scripts/Obstacles/SawTrap.cs
+++ Assets/Scripts/Obstacles/SawTrap.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class SawTrap : Obstacle
 {
+    [SerializeField] private ParticleSystem _hitEffect;
+
     protected override void Activate()
     {
-
+        if (_hitEffect)
+            Instantiate(_hitEffect, transform.position, _hitEffect.transform.rotation);
     }
 }
