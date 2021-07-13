1945fd2783df05dc9ff31b72dc0e21c542083c15
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-07-13 15:53:31 +0300

    Add level Castle_05_1

---
+++ Assets/Scripts/Obstacles/MovingSawTrap.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using SplineMesh;
+
+public class MovingSawTrap : ActiveObject
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private ParticleSystem _destroyEffect;
+
+    private Spline _spline;
+    private Coroutine _moving;
+    private float _distanceCovered;
+    private Direction _direction;
+
+    private void Awake()
+    {
+        _spline = GetComponentInParent<Spline>();
+    }
+
+    private void Start()
+    {
+        transform.position = _spline.GetSampleAtDistance(0).location;
+        _distanceCovered = 0f;
+        _direction = Direction.Right;
+    }
+
+    public override void Action()
+    {
+        _moving = StartCoroutine(Moving());
+    }
+
+    private IEnumerator Moving()
+    {
+        while (true)
+        {
+            _distanceCovered += _speed * Time.deltaTime * (int)_direction;
+
+            if (_distanceCovered >= _spline.Length)
+            {
+                StopCoroutine(_moving);
+                DestroySaw();
+                break;
+            }
+
+            transform.position = _spline.GetSampleAtDistance(_distanceCovered).location;
+
+            yield return null;
+        }
+    }
+
+    private void DestroySaw()
+    {
+        Instantiate(_destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
