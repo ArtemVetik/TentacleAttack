281aac869b55f8549ddc775c7fb6ad1c6b8440d5
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-09-23 23:32:35 +0300

    Add space level

--- Assets/Scripts/Obstacles/CeilingFan.cs
+++ Assets/Scripts/Obstacles/CeilingFan.cs
@@ -6,15 +6,20 @@ public class CeilingFan : ActiveObject
     [SerializeField] private ParticleSystem _disableEffect;
     [SerializeField] private GameObject[] _disablableObjects;
     private Animator _selfAnimator;
+    private MeshRenderer _selfMeshRenderer;
 
     private void Start()
     {
         _selfAnimator = GetComponent<Animator>();
+        _selfMeshRenderer = GetComponent<MeshRenderer>();
     }
 
     public override void Action()
     {
         _selfAnimator.enabled = false;
+
+        if (_selfMeshRenderer != null)
+            _selfMeshRenderer.enabled = false;
 
         foreach (var disabeleObject in _disablableObjects)
         {
---
+++ Assets/Scripts/Obstacles/SpaceTrap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SpaceTrap : ActiveObject
+{
+    [SerializeField] private ParticleSystem _workEffect;
+    [SerializeField] private Material _safeMaterial;
+
+    private MeshRenderer _selfRenderer;
+
+    private void Start()
+    {
+        _selfRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public override void Action()
+    {
+        
+    }
+}
