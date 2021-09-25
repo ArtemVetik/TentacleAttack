5ec2a0f792cc055902f66060c600c3dd386053c1
Cromwill <33699699+Cromwill@users.noreply.github.com>
2021-09-25 18:10:33 +0300

    Add space level

---
+++ Assets/Scripts/Obstacles/LiftObstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LiftObstacle : ActiveObject
+{
+    [SerializeField] private ParticleSystem _disableEffect;
+    [SerializeField] private GameObject _dangerousObject;
+
+    private Animator _selfAnimator;
+
+    private void Start()
+    {
+        _selfAnimator = GetComponent<Animator>();
+    }
+
+    public override void Action()
+    {
+        _selfAnimator.enabled = false;
+
+        Instantiate(_disableEffect, _dangerousObject.transform.position, Quaternion.identity);
+        _dangerousObject.SetActive(false);
+    }
+}
